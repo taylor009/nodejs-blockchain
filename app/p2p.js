860074cf1c45c4b27af731005630fe3fd40bda96
taylor009 <taylor@baremetalcloud.com>
2022-08-04 04:09:57 -0700

    created a message handler function

--- app/p2p.js
+++ app/p2p.js
@@ -27,9 +27,16 @@ class P2pServer {
 
 	connectSocket(socket) {
 		this.sockets.push(socket);
-		console.log(`Socket connected: ${socket.url}`);
-		console.log('Connected to a new peer');
-		socket.on('message', message => this.messageHandler(message, socket));
+		console.log(`Socket connected`);
+		this.messageHandler(socket);
+		socket.send(JSON.stringify(this.blockchain.chain));
+	}
+
+	messageHandler(socket) {
+		socket.on('message', message => {
+			const data = JSON.parse(message);
+			console.log('data', data);
+		});
 	}
 }
 
