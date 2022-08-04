3ee74c853e9f78fe4eae1864699ee26361d9b192
taylor009 <taylor@baremetalcloud.com>
2022-08-04 03:30:25 -0700

    Reorganised project and created getBlock endpoint.

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,6 +1,7 @@
 const Block = require('./block');
 
-class Blockchain {
+class Index
+{
 	constructor() {
 		this.chain = [Block.genesis()];
 	}
@@ -45,4 +46,4 @@ class Blockchain {
 	}
 }
 
-module.exports = Blockchain;
+module.exports = Index;
