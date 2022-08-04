7e6430a05c72c3b72a31d20d6ae05b9c17d99ebd
taylor009 <taylor@baremetalcloud.com>
2022-08-04 02:17:07 -0700

    Created genesis block function to create the first block of the blockchain. also created a mine-block function.

--- block.js
+++ block.js
@@ -17,6 +17,18 @@ class Block {
 		Nonce: ${this.nonce}
 		Difficulty: ${this.difficulty}`;
 	}
+
+	static genesis() {
+		return new this('Genesis time', '-----', 'f1r57-h45h', [], 0, 0);
+	}
+
+	static mineBlock(lastBlock, data) {
+		let timestamp = Date.now();
+		let lastHash  = lastBlock.hash;
+		let hash      = 'todo-hash'
+
+		return new this(timestamp, lastHash, hash, data, 0, 0);
+	}
 }
 
 module.exports = Block;
--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'baz', 'qux', 'quux', 'quuz');
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
 
-console.log(block.toString());
+console.log(fooBlock.toString());
